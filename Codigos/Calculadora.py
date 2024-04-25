f9b247965f5804cb5fb2b0d1e058bce2b950e7ef
guicarbar <guicarbar@gmail.com>
2024-04-25 19:41:33 -0300

    criando o codigo
    foi feita a verificação de virgula e a pergunta de valor investido e de tempo que sera deixado rendendo

--- Codigos/Calculadora.py
+++ Codigos/Calculadora.py
@@ -4,5 +4,33 @@
 
 #area de funçoes
 
+
+def verificar_v(n):
+    atualizado = ''
+    for virgula in n:
+        if virgula == ',':
+            atualizado += '.'
+        else:
+            atualizado += virgula
+    return atualizado
+    
 #area do codigo
+
+print('qual seu investimento inicial ?')
+inv = verificar_v(input())
+
+print('quanto meses vai deixar rendendendo ?')
+print('nn sabe exatamente ? digite N, vamos calcular para vc ?')
+tempox = input()
+
+if tempox.upper() == 'N':
+    print('qunatos anos vai deixar rendendendo ?')
+    tempo_ano = int(input()) * 12
+    print(f'voce vai deixar rendendo por {tempo_ano} rendendo')
+    print('deseja adcionar mais quantos meses ? se ja esta satifesito coloque 0')
+    tempo_meses = int(input())
+    tempo = tempo_ano + tempo_meses
+else:
+    print(f'Vc vai deixar rendendo por {tempox} meses')
+    tempo = tempox
 
