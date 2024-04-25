25b61e200206910ed65028a2f2472b1af5284f75
guicarbar <guicarbar@gmail.com>
2024-04-25 20:38:54 -0300

    avançando no codigo
    estou comproblema para calcular o valor final do mantantes

--- Codigos/Calculadora.py
+++ Codigos/Calculadora.py
@@ -13,11 +13,36 @@ def verificar_v(n):
         else:
             atualizado += virgula
     return atualizado
+
+def inteiro(p):
+    try:
+        int(p)
+        return True
+    except ValueError:
+        return False
+    
+def decimal(p):
+    try:
+        float(p)
+        return True
+    except ValueError:
+        return False
+
+
+def verifcar_n(p):
+    if inteiro(p) == True or decimal(p) == True:
+        print('tudo certo')
+    else:
+        print('isso nao é um numero')
+        
     
 #area do codigo
 
 print('qual seu investimento inicial ?')
-inv = verificar_v(input())
+inv = input()
+
+verificar_v(inv)
+verifcar_n(inv)
 
 print('quanto meses vai deixar rendendendo ?')
 print('nn sabe exatamente ? digite N, vamos calcular para vc ?')
@@ -26,11 +51,38 @@ tempox = input()
 if tempox.upper() == 'N':
     print('qunatos anos vai deixar rendendendo ?')
     tempo_ano = int(input()) * 12
-    print(f'voce vai deixar rendendo por {tempo_ano} rendendo')
+    print(f'voce vai deixar rendendo por {tempo_ano} meses rendendo')
     print('deseja adcionar mais quantos meses ? se ja esta satifesito coloque 0')
     tempo_meses = int(input())
     tempo = tempo_ano + tempo_meses
 else:
-    print(f'Vc vai deixar rendendo por {tempox} meses')
+    verifcar_n(tempox)
     tempo = tempox
+    
 
+print('a taxa de juros e ao mes ou ao ano ? A ou M')
+taxa = input().upper()
+
+if taxa == 'A':
+    print('qual a taxa de juros ao ano ?')
+    jurosx = input()
+    verificar_v(jurosx)
+    verifcar_n(jurosx)
+    jurosx = int(jurosx)
+    juros = jurosx / 12
+elif taxa == 'M':
+    print('qual a taxa de juros ao mes ?')
+    jurosx = input()
+    verificar_v(jurosx)
+    verifcar_n(jurosx)
+    juros = int(jurosx)
+
+
+
+inv = int(inv)
+tempo = int(tempo)
+juros = juros / 100
+
+montante = inv * juros * tempo
+
+print(montante)
