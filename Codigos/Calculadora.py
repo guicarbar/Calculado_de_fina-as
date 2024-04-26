7e6c84dd55298eefa9c4502e4f05dd5b67274b1f
guicarbar <guicarbar@gmail.com>
2024-04-25 21:21:37 -0300

    criando o calculo de simples e composto

--- Codigos/Calculadora.py
+++ Codigos/Calculadora.py
@@ -1,7 +1,5 @@
 #area de imports
 
-#area de variaveis fixas
-
 #area de funçoes
 
 
@@ -33,16 +31,20 @@ def verifcar_n(p):
     if inteiro(p) == True or decimal(p) == True:
         print('tudo certo')
     else:
-        print('isso nao é um numero')
+        print('isso nao é um numero adcionar um reset no codigo depois')
         
     
 #area do codigo
 
+#investimento inicial
+
 print('qual seu investimento inicial ?')
 inv = input()
 
-verificar_v(inv)
+inv = verificar_v(inv)
 verifcar_n(inv)
+
+#quanto tempo
 
 print('quanto meses vai deixar rendendendo ?')
 print('nn sabe exatamente ? digite N, vamos calcular para vc ?')
@@ -58,7 +60,21 @@ if tempox.upper() == 'N':
 else:
     verifcar_n(tempox)
     tempo = tempox
+
+#juros simples ou composto
+
+print('vc gostaria de adcionar mensalmente um valor no seu investimento ? S ou N')
+y = str(input()).upper()
+
+if y == 'S':
+    calc = True
+elif y == 'N':
+    calc = False
+    print('ok vamos continuar')
+else:
+    print('isso nn e um valor valido, adcionar o reset')
     
+#taxa de juros
 
 print('a taxa de juros e ao mes ou ao ano ? A ou M')
 taxa = input().upper()
@@ -66,23 +82,42 @@ taxa = input().upper()
 if taxa == 'A':
     print('qual a taxa de juros ao ano ?')
     jurosx = input()
-    verificar_v(jurosx)
+    jurosx = verificar_v(jurosx)
     verifcar_n(jurosx)
     jurosx = int(jurosx)
     juros = jurosx / 12
 elif taxa == 'M':
     print('qual a taxa de juros ao mes ?')
     jurosx = input()
-    verificar_v(jurosx)
+    jurosx = verificar_v(jurosx)
     verifcar_n(jurosx)
     juros = int(jurosx)
+else:
+    print('isso nn e um numero, adcionar o reset no codigo')
+
+#escolhendo formula
+
+if calc == True:
+    simple()
+else:
+    composto()
+    
 
 
+
+#corrigindo as varivaeis
 
 inv = int(inv)
 tempo = int(tempo)
 juros = juros / 100
 
-montante = inv * juros * tempo
 
-print(montante)
+#calculo simples
+
+rendimento = inv * juros * tempo
+print(f'{inv}apos {tempo} meses rendendo a uma taxa de {juros} seu rendimento foi de {rendimento}')
+rendimento += inv
+print(f'somando um total de {rendimento}')
+
+
+#calculo composto
